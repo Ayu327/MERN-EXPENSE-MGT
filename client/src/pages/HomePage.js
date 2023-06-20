1d126acfe1ebbd5e59fded1d5f34322431a1e559
Ayu327 <86731124+Ayu327@users.noreply.github.com>
2023-06-20 20:25:57 +0530

    modals added

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,30 @@
 
 import Layout from "../components/Layout/Layout"
+import './HomePage.css'
 
 import React from 'react'
+import { useState } from "react"
+import Modal from "antd/es/modal/Modal"
 
 const HomePage = () => {
+  const[showModal,setShowModal] = useState(false)
   return (
     <Layout>
-        <h1>Home Page</h1>
+      <div className="filters">
+    <div>Range Filters</div>
+    <div>
+      <button className="btn btn-primary" onClick={()=>setShowModal(true)}>Add new</button>
+    </div>
+      </div>
+      <div className="content"></div>
+      <Modal title="Add Transaction"
+      open={showModal}
+      onCancel={()=>setShowModal(false)}
+      footer={false}
+      >
+        
+    <h1>Hello</h1>
+      </Modal>
     </Layout>
   )
 }
